22ebd358ac677e31b20f585bbb35a1e26ad59d97
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-07-19 00:55:00 +0300

    update

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,9 @@
+import  cap  from '../../images/cap.png'
+import  firstMember  from '../../images/firstMember.png'
 import { BsBellFill } from 'react-icons/bs';
 import { PiMagnifyingGlassLight } from 'react-icons/pi';
 import style from './Main.module.css';
+
 
 
 export function Main() {
@@ -8,14 +11,50 @@ export function Main() {
         <main className={style.main} >
             <header className={style.header}>
                 <form className={style.right} >
-                    <input className={PiMagnifyingGlassLight} placeholder='Search Hear' type= 'Search' />
+                    <input placeholder='Search here' type= 'Search' />
                 </form>
-                <img className={style.logo} src="/" alt='#' />
+                {/* <img className={style.cap} src={cap} alt='#' /> */}
                 <div className={style.left}>
-                    <BsBellFill size='1.5rem' color='#aaa' />
-                    <img src="/" alt="/" />
+                    <BsBellFill size='1rem' color='#aaa' />
+                    <img src={firstMember} alt="/" />
                 </div>
             </header>
+            <section className={style.course}>
+                <div className={style.title}>
+                    <h1>Course in progress</h1>
+                    <h2>View all</h2>
+                </div>
+                <div className={style.progress}>
+                    <div className={style.apps}>
+                        <BsBellFill size='1rem' color='#aaa' />
+                        <BsBellFill size='1rem' color='#aaa' />
+                        <h1>App design</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem</p>
+                    </div>
+                    <div className={style.web}>
+                        <BsBellFill size='1rem' color='#aaa' />
+                        <BsBellFill size='1rem' color='#aaa' />
+                        <h1>Web design</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem</p>
+                    </div>
+                    <div className={style.dashboard}>
+                        <BsBellFill size='1rem' color='#aaa' />
+                        <BsBellFill size='1rem' color='#aaa' />
+                        <h1>Dashboard</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem</p>
+                    </div>
+                    <div className={style.upgrade}>
+                        <BsBellFill size='1rem' color='#aaa' />
+                        <BsBellFill size='1rem' color='#aaa' />
+                        <h1>Upgrade</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem</p>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
